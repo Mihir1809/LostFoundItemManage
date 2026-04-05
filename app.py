fcd43565f875cf721f96ea745617138e6fcd5a0d
Mihir Dixit <rdixit444@gmail.com>
2026-04-05 20:09:45 +0530

    Fix render port issue

--- app.py
+++ app.py
@@ -198,4 +198,5 @@ def send_message(id):
 # ---------------- RUN ---------------- #
 
 if __name__ == '__main__':
-    app.run(app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False))
+   port = int(os.environ.get("PORT", 10000))
+   app.run(host='0.0.0.0', port=port)
